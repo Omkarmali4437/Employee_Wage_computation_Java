40e50cfe5d5aae0a0082e03f1c676f7f68f5e763
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-21 13:13:00 +0530

    Added logic for Employee attendence

--- empwage.java
+++ empwage.java
@@ -1,5 +1,14 @@
 public class empwagebuilder {
    public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
-   }
+   	int isfulltime=1;
+
+   	int empcheck = (int)Math.floor(Math.random() * 10)%2;
+
+   	if (empcheck == isfulltime)
+      	System.out.println("Employee is present");
+   	else
+     		System.out.println("Employee is absent");
+
+	}
 }
