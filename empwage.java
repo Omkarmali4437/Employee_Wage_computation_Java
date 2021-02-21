195b3bbaebf63a2db1e19befaeab263abe3d0988
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-21 18:47:57 +0530

    Added a condition for maximum working hours and total number of days

--- empwage.java
+++ empwage.java
@@ -1,29 +1,36 @@
-public class empwagebuilder {
-	public static void main(String[] args) {
+public class empwagebuilder { public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
+		//Constants
 		int isfulltime=1;
 		int isparttime=2;
 		int month=20;
-		int dailyhour;
-		int dailywage;
 		int wageperhour=20;
-		int empcheck = (int)Math.floor(Math.random() * 10)%3;
-		switch (empcheck) {
-			case 1:
-				System.out.println("Employee is Full Time");
-				dailyhour=8;
-				dailywage=wageperhour*dailyhour*month;
-				System.out.println("Monthly Wage is: "+dailywage);
+		int maximumhrs=100;
+		int numberofworkingdays=20;
+		//Variables
+      int dailywage;
+      int mwage=0;
+		int totalworkingdays=0;
+		int totalworkhrs=0;
+		while(totalworkingdays<numberofworkingdays && totalworkhrs<maximumhrs )
+		{
+			totalworkingdays++;
+			int dailyhour=0;
+			int empcheck = (int)Math.floor(Math.random() * 10)%3;
+			switch (empcheck) {
+         case 1:
+            dailyhour=8;
 				break;
-			case 2:
-				System.out.println("Emplpyee is Part Time");
-				dailyhour=4;
-				dailywage=wageperhour*dailyhour*month;
-				System.out.println("Monthly Wage is: "+dailywage);
+         case 2:
+            dailyhour=4;
 				break;
-			case 0:
-    			System.out.println("Employee is Absent");
-    			break;
+         case 0:
+            dailyhour=0;
+				break;
+      	}
+			totalworkhrs+=dailyhour;
 		}
+		int totalsalary=totalworkhrs*wageperhour;
+		System.out.println("Total salary of a employee is :"+totalsalary);
 	}
 }
