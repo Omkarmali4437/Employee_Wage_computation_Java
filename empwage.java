7b56d6bd11b170c2d0b9da00dc3b8bfcfb0417c8
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-21 13:41:59 +0530

    Removed if and else logic and replaced it with switch case

--- empwage.java
+++ empwage.java
@@ -1,32 +1,28 @@
 public class empwagebuilder {
-   public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
-   	int isfulltime=1;
+		int isfulltime=1;
 		int isparttime=2;
+		int dailyhour;
+		int dailywage;
 		int wageperhour=20;
-   	int empcheck = (int)Math.floor(Math.random() * 10)%3;
-
-   	if (empcheck == isfulltime)
-      {
-			System.out.println("Employee is Full Time");
-   		int dailyhour=8;
-			int dailywage=wageperhour*dailyhour;
-			System.out.println("Daily Wage is: "+dailywage);
-		}
-		else if (empcheck == isparttime)
-		{
-			System.out.println("Employee is Part Time");
-      	int dailyhour=4;
-       	int dailywage=wageperhour*dailyhour;
-       	System.out.println("Daily Wage is: "+dailywage);
-
-		}
-		else
-		{
-     		System.out.println("Employee is absent");
-			int dailyhour=0;
-         int dailywage=wageperhour*dailyhour;
-         System.out.println("Daily Wage is: "+dailywage);
+		int empcheck = (int)Math.floor(Math.random() * 10)%3;
+		switch (empcheck) {
+			case 1:
+				System.out.println("Employee is Full Time");
+				dailyhour=8;
+				dailywage=wageperhour*dailyhour;
+				System.out.println("Daily Wage is: "+dailywage);
+				break;
+			case 2:
+				System.out.println("Emplpyee is Part Time");
+				dailyhour=4;
+				dailywage=wageperhour*dailyhour;
+				System.out.println("Daily Wage is: "+dailywage);
+				break;
+			case 0:
+    			System.out.println("Employee is Absent");
+    			break;
 		}
 	}
 }
