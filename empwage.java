21cc4e897a8b31e77054a916f7c9d17d95edec91
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-21 13:18:55 +0530

    Added logic for Daily wage calculation

--- empwage.java
+++ empwage.java
@@ -2,13 +2,22 @@ public class empwagebuilder {
    public static void main(String[] args) {
 		System.out.println("Welcome to employee wage problem");
    	int isfulltime=1;
-
+		int dailyhour=8;
    	int empcheck = (int)Math.floor(Math.random() * 10)%2;
 
    	if (empcheck == isfulltime)
-      	System.out.println("Employee is present");
-   	else
+      {
+			System.out.println("Employee is present");
+   		int wageperhour=20;
+			int dailywage=wageperhour*dailyhour;
+			System.out.println("Daily Wage is: "+dailywage);
+		}
+		else
+		{
      		System.out.println("Employee is absent");
-
+			int wageperhour=0;
+         int dailywage=wageperhour*dailyhour;
+         System.out.println("Daily Wage is: "+dailywage);
+		}
 	}
 }
